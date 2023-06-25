3205d587345be748664e4ad2b55750936819998c
Freddy-ux <alfredmuleri569@gmail.com>
2023-06-25 15:08:01 +0300

    Done with Assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
 end
+def create
+    movie = Movie.create!(movie_params)
+    render json: movie, status: :created
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+  end
